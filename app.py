9652b9a2fd9d14d70437a14770ad8307a9609abc
Edmond Chuc <edmond.chuc@outlook.com>
2019-09-09 22:24:07 +1000

    Test code

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from github_webhook import Webhook
 
+import pprint as pp
+
 app = Flask(__name__)
 webhook = Webhook(app)
 
 
 @webhook.hook()
 def on_push(data):
-    print('Received push with: \n{}'.format(data))
+    pp.pprint('Received push with: ')
+    pp.pprint(data)
+
+    print('Repository ID: ', data['repository']['id'])
+    return 'hello'
 
 
 if __name__ == '__main__':
